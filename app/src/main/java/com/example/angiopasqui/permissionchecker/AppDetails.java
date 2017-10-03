b01cc17343429e309623eda7c20a030c53da7ea9
Angelo Settembre <a.settembre8@studenti.unisa.it>
2017-10-03 17:08:59 +0200

    Add group classification app in AppDetails 2

--- app/src/main/java/com/example/angiopasqui/permissionchecker/AppDetails.java
+++ app/src/main/java/com/example/angiopasqui/permissionchecker/AppDetails.java
@@ -93,34 +93,10 @@ public class AppDetails extends Activity {
         try {
             pi = pm.getPackageInfo(packageName, PackageManager.GET_PERMISSIONS);
             String[] perm = pi.requestedPermissions;
-            List<String> permessiAll = new ArrayList<String>();
-            if (perm != null) {
-                for (String a : perm) {
-                    permessiAll.add(a);
-                }
-            }
 
-            PermissionInfo[] permUser = pi.permissions;
-            List<String> permessiApp = new ArrayList<String>();
-            if (permUser != null) {
-                for (PermissionInfo s : permUser) {
-                    permessiApp.add(s.name);
-                }
-            }
-
-            for (int z = 0; z < permessiAll.size(); z++) {
-                String p = permessiAll.get(z);
-                for (int j = 0; j < permessiApp.size(); j++) {
-                    String a = permessiApp.get(j);
-                    if (p.equals(a)) {
-                        permessiAll.remove(p);
-                        break;
-                    }
-                }
-            }
-            for (String prova : permessiAll) {
-                System.out.println("PROVAAA 555: " + prova);
-                pgi = pm.getPermissionInfo(prova, 0);
+            for (int j = 0; j < perm.length; j++) {
+                System.out.println("PROVAAA 555: " + perm[j]);
+                pgi = pm.getPermissionInfo(perm[j], 0);
                 System.out.println("PROVAAA 888: " + pgi.group);
                 if(pgi.group != null){
                     arrayAdapter.add(pgi.group);
